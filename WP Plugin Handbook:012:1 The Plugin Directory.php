cf9aaca5698acd5d725c0b538c83c61edd86b694
CreationTribe <creationtribe@users.noreply.github.com>
2021-01-27 18:56:47 -0700

    finished plugin guidelines and planning

---
+++ WP Plugin Handbook:012:1 The Plugin Directory.php
@@ -0,0 +1,85 @@
+<?php
+# PLUGIN DIRECTORY
+/*
+ * 1. Must be GNU GPL v2
+ * 2. must use WP's Subversion to submit and maintain
+ * 3. Any "powered-by" links or advertising must be clearly documented & explicit user permission granted
+ * 4. Nothing dishonest, immoral, or offensive
+ */
+
+# THE PLUGIN DIRECTORY
+/*
+ * - Suggestions to improve guidelines: plugins@wordpress.org
+ * * ALL contact information MUST be kept up to date
+ *    |-> AUTO REPLY EMAILS OR SUPPORT SYSTEM QUEUES ARE NOT PERMITTED
+ *        * Must be an email that allow easy access to developer
+ * * ALL code must be as secure as possible
+ */
+
+# PLUGIN GUIDELINES
+/*
+ * 1. GNU GPL v2 COMPATIBILITY =OR= GPU GPL COMPATIBLE LICENSE
+ *     |-> all libraries and dependencies must be GPL as well
+ *     |-> List of GPU GPL v2 compatible licenses: https://www.gnu.org/licenses/license-list.html#GPLCompatibleLicenses
+ * 2. DEVELOPERS ARE RESPONSIBLE FOR CONTENTS AND ACTIONS OF THEIR PLUGIN
+ *     |-> Must verify GPU GPL compatibility of all libraries & code used
+ * 3. STABLE VERSION MUST BE AVAILABLE FROM WP PLUGIN DIRECTORY PAGE
+ * 4. CODE MUST BE HUMAN READABLE
+ *     |-> No obfuscation
+ *     |-> Must provide access to code, whether in deployed plugin itself, or ...
+ *     |-> a link in the readme to dev location
+ * 5. TRIAL WARE IS NOT PERMITTED
+ * 6. SASS IS PERMITTED
+ * 7. PLUGINS MAY NOT TRACK USERS W/OUT CONSENT
+ * 8. PLUGINS MAY NOT SEND EXECUTABLE CODE VIA 3RD PARTY SYSTEMS
+ *     |-> Basically, don't install or load anything that is not inherent plugin shit
+ *     |-> No auto premium updates (must do this through wp/subversion
+ *     |-> No CDN's other than fonts (including css & js)
+ *     |-> No using 3rd party to update data if not explicitly permitted by TOS
+ *     |-> No iFrames to connect admin pages - always use APIs
+ * 9. NOTHING ILLEGAL, MORALLY OFFENSIVE, DISHONEST ... you get the gist
+ *     |-> No WORDCAMP or FORUM GUIDELINE violations
+ * 10. DO NOT EMBED EXTERNAL LINKS OR CREDITS ON PUBLIC SITE W/OUT USER PERMISSION
+ *     |-> All "powered by" or credits must be explicitly allowed by site admin
+ * 11. DON'T HIJACK THE ADMIN DASHBOARD
+ * 12. NO SPAMMING OF PUBLIC FACING PAGES
+ * 13. PLUGINS MUST USE WP DEFAULT LIBS
+ *     |-> Any of these must be used from WP - don't import your own: https://developer.wordpress.org/reference/functions/wp_enqueue_script/#notes
+ * 14. FREQUENT COMMITS TO A PLUGIN SHOULD BE AVOIDED
+ *     |-> The WP SVN is a release repo - NOT A DEV REPO
+ *     |-> The only exception is  readme updates to indicate support for latest WP releases
+ * 15. PLUGIN VERSION NUMBERS MUST BE INCREMENTED FOR EACH NEW RELEASE
+ *     |-> For further reading: https://developer.wordpress.org/plugins/wordpress-org/how-to-use-subversion/#tags
+ *     |-> and ... https://developer.wordpress.org/plugins/wordpress-org/how-your-readme-txt-works/
+ * 16. COMPLETE PLUGIN MUST BE AVAILABLE AT TIME OF SUBMISSION
+ * 17. RESPECT TRADEMARKS, COPYRIGHTS AND PROJECT NAMES
+ * 18. WP RESERVES THE RIGHT TO MAINTAIN THE PLUGIN DIRECTORY TO THE BEST OF THEIR ABILITIES
+ *     WP reserves the right to:
+ *     |-> Update guidelines at any time
+ *     |-> Remove any plugin for any reason - even if not explicitly covered by guidelines
+ *     |-> Grant exceptions - allowing developers time to address issues, even security issues
+ *     |-> Remove developer access to a plugin in lieu of a new active dev
+ *     |-> Make changes to a plugin w/out dev consent in interest of public safety
+ */
+
+# PLANNING YOUR PLUGIN (PLANNING, SUBMITTING, & MAINTAINING)
+/*
+ * 1. TEST & TEST AGAIN
+ * 2. PICK A GOOD NAME
+ *     |-> Slug cannot be changed later
+ * 3. WRITE GREAT DOCUMENTATION
+ *     |-> Add README (description / installation instructions / support directions)
+ * 4. SUBMIT PLUGIN
+ *     |-> Register on wordpress.org
+ *     |-> whitelist plugins@wordpress.org
+ *     |-> submit plugin w/ brief overview with COMPLETE zip file of plugin
+ * 5. PUSH OUT FIRST VERSION
+ * 6. EMBRACE OPENSOURCE
+ *     |-> Basically, just use GitHub
+ * 7. LISTEN TO YOUR USERS
+ *     |-> Use the WP support forum
+ * 8. REGULARLY PUSH NEW VERSIONS
+ *     |-> Balance between too many and too few updates
+ * 9. RINSE AND REPEAT
+ */
+
